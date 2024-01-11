9f9b7a7beda38a6ee635406633444b3381021ffa
Oussama Alaoui chrifi <oalaoui-@e2r9p10.1337.ma>
2024-01-11 21:26:06 +0100

    fixed problem in reverse ios plate

--- screen/svg_assets/basic.js
+++ screen/svg_assets/basic.js
@@ -134,11 +134,27 @@ export default function Basic({style, alpha, number, w}) {
                                     let number = '`+number+`'
                                     document.getElementById("english-number").innerHTML = number;
                                     document.getElementById("arabic-number").innerHTML = toArabicNumber(number);
-                            
+                                    
+                                    function reverseString(str) {
+                                        // Step 1. Use the split() method to return a new array
+                                        var splitString = str.split(""); // var splitString = "hello".split("");
+                                        // ["h", "e", "l", "l", "o"]
+                                     
+                                        // Step 2. Use the reverse() method to reverse the new created array
+                                        var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+                                        // ["o", "l", "l", "e", "h"]
+                                     
+                                        // Step 3. Use the join() method to join all elements of the array into a string
+                                        var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+                                        // "olleh"
+                                        
+                                        //Step 4. Return the reversed string
+                                        return joinArray; // "olleh"
+                                    }
                             
                                     let alpha = '`+alpha+`'
                                     document.getElementById("english-alpha").innerHTML = alpha.toUpperCase();
-                                    document.getElementById("arabic-alpha").innerHTML = toArabicAlpha(alpha.toUpperCase());
+                                    document.getElementById("arabic-alpha").innerHTML = reverseString(toArabicAlpha(alpha.toUpperCase()));
                         
                                 </script>
                         </svg>
